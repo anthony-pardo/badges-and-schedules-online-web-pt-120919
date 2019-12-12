99e303dd6db9d5ae4ec0d11672a05455604bc80b
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-12 16:27:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  assign_rooms(attendees).each do |room|
-    puts room
+  assign_rooms(attendees).each do |room_assignment|
+    puts room_assignment
   end
 end
